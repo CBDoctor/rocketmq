b22d026cad13cadc9d353ee55c2995e4abac37c7
cuiyanwei <cuiyanwei@kuaishou.com>
2021-04-18 22:34:08 +0800

    测试

--- srvutil/src/main/java/org/apache/rocketmq/srvutil/FileWatchService.java
+++ srvutil/src/main/java/org/apache/rocketmq/srvutil/FileWatchService.java
@@ -34,6 +34,7 @@ import org.apache.rocketmq.logging.InternalLogger;
 import org.apache.rocketmq.logging.InternalLoggerFactory;
 
 public class FileWatchService extends ServiceThread {
+    //测试
     private static final InternalLogger log = InternalLoggerFactory.getLogger(LoggerName.COMMON_LOGGER_NAME);
 
     private final List<String> watchFiles;
